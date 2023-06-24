1ac48aa259e27f98b5b305028e4b326a42c6b859
Xavier DMello <xaviermdmello@gmail.com>
2023-06-24 15:18:53 -0400

    connect wallet button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+// Rainbowkit imports
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { gnosis, polygon } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([gnosis, polygon], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "PNYX",
+  projectId: "2086d045ff3357c4cb49f376d3b4ac7a",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        modalSize="compact"
+      >
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
