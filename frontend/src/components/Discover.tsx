1eb98bf1b1147e9cbd2f4740d0f88ee2614fb60b
Xavier DMello <xaviermdmello@gmail.com>
2023-06-24 23:48:13 -0400

    spline test

--- frontend/src/components/Discover.tsx
+++ frontend/src/components/Discover.tsx
@@ -1,9 +1,10 @@
 import "../styles/Discover.css"
+import Spline from "@splinetool/react-spline";
 
 export default function Discover() {
     return (
-        <div className="Discover">
-            Discover
-        </div>
-    )
+      <div className="Discover">
+        <Spline className="cube" scene="https://prod.spline.design/lCq1bZmumpwTeaZN/scene.splinecode" />
+      </div>
+    );
 }
